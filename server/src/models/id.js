59fc2509649d258649bc8711265395b8d5d6d9bc
emanuelosva <emanuelosva@gmail.com>
2020-12-10 20:52:44 -0600

    :bug: fix id function

--- server/src/models/id.js
+++ server/src/models/id.js
@@ -1,12 +1,10 @@
-const { func } = require("joi");
-
 /**
  * Insert the fiel "id" in mongo schema and create an index.
  */
 function createId() {
   if (this.isModified('_id')) {
     this.id = this._id
-    this.index('id')
+    this.index({ id: 1 })
   }
 }
 
