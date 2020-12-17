6a56c6e1ad08a7b09b5fa284ec9bf59d937eac20
emanuelosva <emanuelosva@gmail.com>
2020-12-16 20:30:47 -0600

    :construction: implement admin creation

--- src/api/users/controller.js
+++ src/api/users/controller.js
@@ -2,14 +2,48 @@ const { response } = require('../../core')
 const UserService = require('./service')
 const { User, RefreshToken } = require('./models')
 const { Controller } = require('../../core')
+const { scopes } = require('../../auth')
 
 const userService = new UserService(User, RefreshToken)
 
 class UserController extends Controller {
   constructor(service = userService) {
     super(service, { serviceName: 'user' })
-    this.login = this.login.bind(this)
     this.refreshToken = this.refreshToken.bind(this)
+    this.login = this.login.bind(this)
+    this.signup = this.signup.bind(this)
+    this.createAdmin = this.createAdmin.bind(this)
+    this.createSuperAdmin = this.createSupeAdmin.bind(this)
+  }
+
+  async createSuperAdmin(req, res, next) {
+    try {
+      const { body: userDTO } = req
+      const data = await this.service.insert({ ...userDTO, type: scopes.superAdmin })
+      return response.success(req, res, 201, data, 'user created')
+    } catch (error) {
+      return next(error)
+    }
+  }
+
+  async createAdmin(req, res, next) {
+    try {
+      const { body: userDTO } = req
+      const data = await this.service.insert({ ...userDTO, type: scopes.admin })
+      return response.success(req, res, 201, data, 'user created')
+    } catch (error) {
+      return next(error)
+    }
+  }
+
+  async signup(req, res, next) {
+    try {
+      const { body: userDTO } = req
+      const data = await this.service.signup(userDTO)
+      return response.success(req, res, 201, data, 'user created')
+    } catch (error) {
+      return next(error)
+    }
   }
 
   async login(req, res, next) {
