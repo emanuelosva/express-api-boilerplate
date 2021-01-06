e90b139320616316819659f68d2de621efffbd3e
emanuelosva <emanuelosva@gmail.com>
2021-01-05 22:30:02 -0600

    :hammer: refactor to class

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,16 +1,3 @@
-const scopes = require('./scopes')
-const jwt = require('./jwt')
-const tokenTypes = require('./tokenTypes')
-
-const createToken = async (
-  email,
-  scope = scopes.user,
-  type = tokenTypes.auth,
-) => jwt.sign({ email, scope, type })
-
-module.exports = {
-  scopes,
-  jwt,
-  createToken,
-  tokenTypes,
-}
+module.exports.scopes = require('./scopes')
+module.exports.JWT = require('./jwt')
+module.exports.tokenTypes = require('./tokenTypes')
--- src/auth/jwt.js
+++ src/auth/jwt.js
@@ -1,35 +1,51 @@
 /**
- * @fileoverview JWT implementation
+ * JWT class implemntation.
+ * ------------------------
+ *
+ * Perform all actions to create and
+ * validate correctly JWTs.
  */
 
 const jwt = require('jsonwebtoken')
-const { promisify } = require('util')
-const config = require('../config')
 const { ApiError } = require('../lib')
+const config = require('../config')
 
-exports.sign = async (payload, expiration) => {
-  const DEFAULT_EXPIRATION_TIME = '1 day'
+class JWT {
+  static DEFAULT_EXPIRATION_TIME = '3 hours'
 
-  if (!payload.email || !payload.type || !payload.scope) {
-    throw new Error('Payload must contain properties: [email, type, scope]')
-  }
-  const sign = promisify(jwt.sign)
-  const token = await sign(payload, config.security.SECRET, {
-    algorithm: config.security.ALGORITHMS[0],
-    expiresIn: expiration || DEFAULT_EXPIRATION_TIME,
-  })
-  return token
-}
-
-exports.verify = async (token) => {
-  const verify = promisify(jwt.verify)
-  try {
-    const payload = await verify(token, config.security.SECRET, {
-      algorithms: config.security.ALGORITHMS,
+  static async sign(payload, expiration = JWT.DEFAULT_EXPIRATION_TIME) {
+    return new Promise((resolve, reject) => {
+      try {
+        if (!payload.email || !payload.scope) {
+          throw new Error('email and scope are needed')
+        }
+        payload.type = payload.type ? payload.type : tokenTypes.AUTH
+        const token = jwt.sign(payload, config.secutity.SECRET, {
+          algorithm: config.security.ALGORITHMS[0],
+          expiresIn: expiration,
+        })
+        return resolve(token)
+      } catch (error) {
+        return reject(error)
+      }
     })
-    return payload
-  } catch (error) {
-    if (error instanceof jwt.TokenExpiredError) ApiError.forbidden('token expired')
-    ApiError.unauthorized()
+  }
+
+  static async verify(token) {
+    return new Promise((resolve) => {
+      try {
+        const payload = jwt.verify(token, config.secutity.SECRET, {
+          algorithms: config.security.ALGORITHMS,
+        })
+        return resolve(payload)
+      } catch (error) {
+        if (error instanceof jwt.TokenExpiredError) {
+          ApiError.raise.forbidden('token expired')
+        }
+        ApiError.raise.unauthorized()
+      }
+    })
   }
 }
+
+module.exports = JWT
